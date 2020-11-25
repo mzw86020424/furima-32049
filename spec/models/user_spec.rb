0c286c771ca2b7d555f262aa8c7d7a8d53d5d2c3
mzw86020424 <mzw86020424@gmail.com>
2020-11-25 19:08:18 +0900

    フォームのリンク修正・不足テストコードの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,6 +22,11 @@ describe User do
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
+      it 'emailに@がついていない場合は登録できない' do
+        @user.email = "abc.com"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
       it '重複したemailが存在する場合登録できない' do
         @user.save
         @second_user = FactoryBot.build(:user)
