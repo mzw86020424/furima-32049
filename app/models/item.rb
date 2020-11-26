bad7446a7d727f26ba2ea40cc71515ba4190deed
mzw86020424 <mzw86020424@gmail.com>
2020-11-26 17:25:20 +0900

    商品情報保存機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,12 @@
 class Item < ApplicationRecord
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :status, :shipping_fee_status, :prefecture, :lead_time
 
   belongs_to :user
   # has_one :transaction
+  has_one_attached :image
 
-  # 空の投稿を保存できないようにする
-  validates :user, :product_name, :introduction, presence: true
-
-  # ジャンルの選択が「--」の時は保存できないようにする
+  validates :user, :product_name, :introduction, :price, presence: true
   validates :category_id, :status_id, :shipping_fee_status_id, :prefecture_id, :lead_time_id, numericality: { other_than: 1 }
 end
