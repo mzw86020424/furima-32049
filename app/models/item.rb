bdf35e13c4f1aee539895284ec0b8e62b34e8442
mzw86020424 <mzw86020424@gmail.com>
2020-11-27 14:44:34 +0900

    商品画像添付のバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,6 @@ class Item < ApplicationRecord
   # has_one :transaction
   has_one_attached :image
 
-  validates :user, :product_name, :introduction, :price, presence: true
+  validates :user, :product_name, :introduction, :price, :image, presence: true
   validates :category_id, :status_id, :shipping_fee_status_id, :prefecture_id, :lead_time_id, numericality: { other_than: 1 }
 end
