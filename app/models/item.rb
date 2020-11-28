742f028f3573e31658d2d97e17b662acba125f07
mzw86020424 <mzw86020424@gmail.com>
2020-11-28 12:20:22 +0900

    itemモデルの単体テストコード実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   # has_one :transaction
   has_one_attached :image
   with_options presence: true do
-    validates :price, numericality: { greater_than: 299, less_than: 10000000 }
+    validates :price, numericality: { greater_than: 299, less_than: 10000000 }, format: { with: /\d/, message: 'is invalid. Input half-width numbers.' }
     validates :user, :product_name, :introduction, :image
     validates :category_id, :status_id, :shipping_fee_status_id, :prefecture_id, :lead_time_id, numericality: { other_than: 1 }
   end
