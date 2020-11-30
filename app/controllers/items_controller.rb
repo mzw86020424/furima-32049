21a2b7a5cd13ad25b277c9197734438e655cd4a1
mzw86020424 <mzw86020424@gmail.com>
2020-11-30 14:34:49 +0900

    直接URLを入力したときの挙動を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]  
+    
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -23,6 +25,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
   end
 
   def update
