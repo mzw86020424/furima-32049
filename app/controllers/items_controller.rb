96cebc41d6fcb419b33043ffee5f9a9d214805ae
mzw86020424 <mzw86020424@gmail.com>
2020-11-30 16:38:58 +0900

    レビュー内容の反映

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -35,7 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
+    @item.destroy if current_user == @item.user
     redirect_to root_path
   end
 
