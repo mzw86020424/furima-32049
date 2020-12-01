35220ae1f5a6a64b70a7a295e58fafffcb743575
mzw86020424 <mzw86020424@gmail.com>
2020-12-01 17:18:44 +0900

    サーバー〜PAY.JS間の決済機能を実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_form = OrderForm.new(order_params)
     if @order_form.valid?
+      pay_item
       @order_form.save
       redirect_to root_path
     else
@@ -18,6 +19,15 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_form).permit(:addresses, :building, :phone, :postal_code, :city, :prefecture_id).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:order_form).permit(:addresses, :building, :phone, :postal_code, :city, :prefecture_id).merge(token: params[:token], user_id: current_user.id, item_id: @item.id, price: @item.price)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_b6d024c85f6d03ef99a58e3e"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,9 +1,9 @@
 class OrderForm
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :addresses, :building, :phone, :postal_code, :city, :prefecture_id, :order  
+  attr_accessor :user_id, :item_id, :addresses, :building, :phone, :postal_code, :city, :prefecture_id, :order, :token, :price
 
   with_options presence: true do
-    validates :user_id, :item_id, :addresses, :city, :prefecture_id
+    validates :user_id, :item_id, :addresses, :city, :prefecture_id, :token, :price
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid.' }
     validates :phone, format: { with: /\A\d{10,11}\z/, message: 'is invalid.' }
   end
