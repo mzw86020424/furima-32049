c01b767a0936c970230f39ba7ff92d7639ff96c6
mzw86020424 <mzw86020424@gmail.com>
2020-12-01 12:32:27 +0900

    ordersコントローラーのcreateアクションをいったん設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,17 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @order = Order.new
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.save
+  end
+
+  private
+
+  def order_params
+    params.require(:order).merge(user_id: current_user.id)
   end
 end
